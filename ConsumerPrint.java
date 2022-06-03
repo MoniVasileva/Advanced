c78c80b05e94d52b3a68d26829773840d1b79993
MoniVasileva <95421319+MoniVasileva@users.noreply.github.com>
2022-06-03 14:23:14 +0300

    Add files via upload

---
+++ ConsumerPrint.java
@@ -0,0 +1,16 @@
+package FunctionalProgramming.exercise;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class ConsumerPrint {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> input = Arrays.stream(scanner.nextLine().split("\\s+")).collect(Collectors.toList());
+        Consumer<String> output = System.out::println;
+        input.forEach(output);
+    }
+}
